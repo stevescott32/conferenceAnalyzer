e9857bcb2d183c3573d3fc044f3fdffd5ec4b07c
Steven <steven.scott@aggiemail.usu.edu>
2019-12-19 10:17:58 -0700

    Starting analyzer project

---
+++ src/analyze/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace analyze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting analyzer");
+        }
+    }
+}
