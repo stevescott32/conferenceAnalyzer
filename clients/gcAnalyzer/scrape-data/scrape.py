8e033cf3d3ea3ae7c71bfe3bee6b36e5d241158c
Steven <steven.scott@aggiemail.usu.edu>
2020-07-14 23:33:38 -0600

    Scraped one talk using selenium and python rather than code snippet

--- clients/gcAnalyzer/scrape-data/scrape.py
+++ clients/gcAnalyzer/scrape-data/scrape.py
@@ -5,8 +5,24 @@ print('Starting scrape script')
 # TODO use a relative instead of this absolute path
 DRIVER_PATH = "/Users/steven/Documents/personal/projects/text-analyzer/lib/webdrivers/geckodriver"
 driver = webdriver.Firefox(executable_path=DRIVER_PATH)
-driver.get('https://lds.org')
 
-driver.close()
+driver.get('https://www.churchofjesuschrist.org/study/general-conference/2019/10/11holland?lang=eng')
+
+titleEle = driver.find_elements_by_xpath("//h1[@id='title1']")
+title = titleEle[0].text
+
+authorEle = driver.find_elements_by_class_name("author-name")
+author = authorEle[0].text
+
+paragraphsEles = driver.find_elements_by_xpath("//div[@class='body-block']//p")
+allText = ''
+paragraphs = []
+for e in paragraphsEles:
+    paragraphs.append(e.text)
+
+for p in paragraphs:
+    print(p)
+
+driver.quit()
 
 print('Successfully completed scrape script')
