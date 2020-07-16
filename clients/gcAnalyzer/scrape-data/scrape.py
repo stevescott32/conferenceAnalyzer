154db3e3a1f623b6f048bc9d1db2a6966cd1b7ae
Steven <steven.scott@aggiemail.usu.edu>
2020-07-15 23:29:41 -0600

    Can scrape one conference but stale element issue

--- clients/gcAnalyzer/scrape-data/scrape.py
+++ clients/gcAnalyzer/scrape-data/scrape.py
@@ -1,28 +1,67 @@
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.firefox.options import Options
 
 print('Starting scrape script')
 
 # TODO use a relative instead of this absolute path
 DRIVER_PATH = "/Users/steven/Documents/personal/projects/text-analyzer/lib/webdrivers/geckodriver"
-driver = webdriver.Firefox(executable_path=DRIVER_PATH)
 
-driver.get('https://www.churchofjesuschrist.org/study/general-conference/2019/10/11holland?lang=eng')
+# setup the driver
+options = Options()
+options.headless = False
 
-titleEle = driver.find_elements_by_xpath("//h1[@id='title1']")
-title = titleEle[0].text
+driver = webdriver.Firefox(options=options, executable_path=DRIVER_PATH)
+wait = WebDriverWait(driver, 10)
 
-authorEle = driver.find_elements_by_class_name("author-name")
-author = authorEle[0].text
+# define reusable element selectors
+card_selector = 'a[href*="study/general-conference/"]'
+talk_selector = 'a[href*="study/general-conference/"].item-3cCP7:not(.sectionTitle-27yfi)'
 
-paragraphsEles = driver.find_elements_by_xpath("//div[@class='body-block']//p")
-allText = ''
-paragraphs = []
-for e in paragraphsEles:
-    paragraphs.append(e.text)
+driver.get('https://www.churchofjesuschrist.org/study/general-conference?lang=eng')
 
-for p in paragraphs:
-    print(p)
 
+def visit_all_cards():
+    print('visiting all cards')
+    conference_content = driver.find_elements_by_class_name('manifest')
+    cards = driver.find_elements_by_css_selector(card_selector)
+
+    if len(conference_content) > 0:
+        visit_all_talks()
+    else:
+        for card in cards:
+            ref = card.get_attribute("href")
+            print(ref)
+            driver.get(ref)
+            visit_all_cards()
+
+
+def visit_all_talks():
+    print('visiting all talks')
+    talks = driver.find_elements_by_css_selector(talk_selector)
+    for talk in talks:
+        talk.click()
+        scrape_one_talk()
+
+
+def scrape_one_talk():
+    print('scraping one talk')
+    #title_ele = driver.find_elements_by_xpath("//h1[@id='title1']")
+    #title = title_ele[0].text
+
+    #author_ele = driver.find_elements_by_class_name("author-name")
+    #author = author_ele[0].text
+
+    paragraph_elements = driver.find_elements_by_xpath("//div[@class='body-block']//p")
+    paragraphs = []
+    for e in paragraph_elements:
+        paragraphs.append(e.text)
+
+    for p in paragraphs:
+        print(p)
+
+
+visit_all_cards()
 driver.quit()
 
 print('Successfully completed scrape script')
