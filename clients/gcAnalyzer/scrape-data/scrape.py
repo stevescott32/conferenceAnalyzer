0e5469085822d024bd5a9fd8d7aab1b068209496
Steven <steven.scott@aggiemail.usu.edu>
2020-07-24 00:05:47 -0600

    Added comments to functions in scrape.py

--- clients/gcAnalyzer/scrape-data/scrape.py
+++ clients/gcAnalyzer/scrape-data/scrape.py
@@ -41,6 +41,13 @@ def scrape_gen_conf():
     driver.get("https://www.churchofjesuschrist.org/study/general-conference?lang=eng")
 
     def visit_all_cards():
+        """Function to recursively visit all cards.
+
+        Gather all hrefs from the visible cards on a page. Iterate through these
+        links. At each page visited while iterating through the links, check
+        whether conference content is visible. If so, call visit_all_talks().
+        If not, recursively call self.
+        """
         global ready_to_quit
         if ready_to_quit:
             return
@@ -62,6 +69,13 @@ def scrape_gen_conf():
                 visit_all_cards()
 
     def visit_all_talks():
+        """ Iterate through all talks in this session of conference.
+
+        This method assumes that the web driver has already navigated to a
+        page with a session of conference displayed. Iterate through all
+        displayed talks in this session of conference, calling scrape_one_talk
+        on each one.
+        """
         global talks_before_early_quit
         global ready_to_quit
         talks = driver.find_elements_by_css_selector(talk_selector)
@@ -74,6 +88,11 @@ def scrape_gen_conf():
             scrape_one_talk()
 
     def scrape_one_talk():
+        """ Scrape the content, title, and author of one conference talk.
+
+        This method assumes that the web driver has already navigated to
+        the page where the target talk is.
+        """
         global debug
         title = ""
         author = ""
