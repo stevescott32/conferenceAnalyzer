67e6f9b5ee5089945a13897249ab7748cbe539e4
Steven <steven.scott@aggiemail.usu.edu>
2020-08-10 12:16:51 -0600

    Added sentiment analysis

--- clients/gcAnalyzer/scrape-data/main.py
+++ clients/gcAnalyzer/scrape-data/main.py
@@ -5,8 +5,15 @@ import textblob
 print('Starting main')
 talks = scrape.scrape_gen_conf()
 
-print(talks[0]["content"])
 blob = textblob.TextBlob(talks[0]["content"])
-print("Nouns phrases:")
-for np in blob.noun_phrases:
-    print(np)
+# print("Noun Phrases: ")
+# for np in blob.noun_phrases:
+#    print(np)
+
+print("Sentiment analysis")
+for talk in talks:
+    blob = textblob.TextBlob(talk["content"])
+    for s in blob.sentences:
+        print(s)
+        sentence_blob = textblob.TextBlob(str(s))
+        print(sentence_blob.sentiment)
